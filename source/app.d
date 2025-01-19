b0c6788a76d399ddd93e015d1c96d7b2e59f4981
PHandradee <63529201+PHandradee@users.noreply.github.com>
2025-01-19 13:42:03 -0300

    Complete Hello-Triangle exercise 02

--- source/app.d
+++ source/app.d
@@ -1,7 +1,10 @@
 //import source._01_getting_started._01_hello_window;
 //import source._01_getting_started._02_01_hello_triangle;
 //import source._01_getting_started._02_02_hello_triangle;
-import _01_getting_started._02_03_hello_triangle;
+//import _01_getting_started._02_03_hello_triangle;
+
+import _01_getting_started._02_04_hello_triangle;
+
 import std.stdio : writefln;
 import libloader;
 
