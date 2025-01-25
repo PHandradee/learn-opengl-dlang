19911951d4b7c82eb55e0b803c873ea42edf19ef
PHandradee <63529201+PHandradee@users.noreply.github.com>
2025-01-25 16:35:00 -0300

    Complete Exercise 03

--- source/app.d
+++ source/app.d
@@ -12,8 +12,9 @@
 //import _01_getting_started._03_04_shaders ;
 //import _01_getting_started._03_05_shaders ;
 
-import _01_getting_started._03_06_shaders ;
+//import _01_getting_started._03_06_shaders ;
 
+import _01_getting_started._03_07_shaders ;
 
 import std.stdio : writefln;
 import libloader;
